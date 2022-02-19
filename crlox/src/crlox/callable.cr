5a59887fae8651a5607977c459a31456203f8459
Matthew Berry <me@mattrb.com>
2022-02-19 11:49:25 -0800

    crlox: pull LoxCallable into its own file

---
+++ crlox/src/crlox/callable.cr
@@ -0,0 +1,44 @@
+module Crlox
+  abstract class LoxCallable
+    getter arity : Int32
+
+    def initialize(@arity : Int32)
+    end
+
+    abstract def call(interpreter : Interpreter, arguments : Array(LoxValue)) : LoxValue
+
+    class Clock < LoxCallable
+      def initialize : Nil
+        super(0)
+      end
+
+      def call(interpreter : Interpreter, arguments : Array(LoxValue)) : Float64
+        Time.utc.to_unix_f
+      end
+
+      def to_s : String
+        "<native fn clock>"
+      end
+    end
+
+    class LoxFunction < LoxCallable
+      def initialize(@declaration : Stmt::Function, @closure : Environment) : Nil
+        super(@declaration.params.size)
+      end
+
+      def call(interpreter : Interpreter, arguments : Array(LoxValue)) : LoxValue
+        environment = Environment.new(@closure)
+        @declaration.params.zip(arguments) do |param, arg|
+          environment.define(param.lexeme, arg)
+        end
+        interpreter.execute_block(@declaration.body, environment)
+      rescue e : Return
+        e.value
+      end
+
+      def to_s : String
+        "<fn #{@declaration.name.lexeme}>"
+      end
+    end
+  end
+end
